6f1ef0368e2a2f90ad2d415f60c052af82b8feba
tahammalik <tahammalik.dev@gmail.com>
2026-06-08 18:05:11 +0530

    change file name user_auth to authentication

---
+++ app/api/v1/authentication.py
@@ -0,0 +1,97 @@
+"""
+This is file handle authentication router
+"""
+
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.core.db import db_dependency
+from app.core.dependencies import get_current_user
+from app.core.exceptions import EmailAlreadyExistsError
+from app.core.logging_config import get_logger
+from app.core.security import create_access_token, hash_password
+from app.models.user_model import User
+from app.schemas.token_schema import Token
+from app.schemas.user_schema import UserCreate, UserResponse
+from app.services.user_service import authenticate_user, find_email
+
+# from app.core.dependencies import require_role, get_current_user
+
+logger = get_logger(__name__)
+
+router = APIRouter(prefix="/v1/auth", tags=["Authentication"])
+
+
+# Signup endpoint for creating a new user
+@router.post(
+    "/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED
+)
+async def create_user(user_data: UserCreate, db: db_dependency):
+
+    email = await find_email(user_data.email, db=db)
+    if email:
+        raise EmailAlreadyExistsError(message="email already exist")
+    new_user = User(
+        username=user_data.username,
+        email=user_data.email,
+        password=hash_password(user_data.password),
+    )
+
+    try:
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
+        logger.info(f"user created {new_user.username}")
+        return new_user
+
+    except Exception as e:
+        logger.error("DB ERROR: %s", e, exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Database error occurred",
+        )
+
+
+# Login endpoint for authenticating users and returning a JWT token
+@router.post("/login", response_model=Token)
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db=db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": str(user.id)})
+    return Token(access_token=access_token.decode("utf-8"), token_type="bearer")
+
+
+@router.post("/token")
+async def token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+
+    user = authenticate_user(form_data.username, form_data.password, db=db)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = create_access_token(data={"sub": str(user.id)})
+
+    return Token(access_token=access_token.decode("utf-8"), token_type="bearer")
+
+
+@router.get("/me", response_model=UserResponse)
+async def get_me(current_user: Annotated[User, Depends(get_current_user)]):
+    """
+    Get current authenticated user information
+    """
+    return current_user
