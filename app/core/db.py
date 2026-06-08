b08afd484d67fc770e29b4f1fcddee3c94db68a2
tahammalik <tahammalik.dev@gmail.com>
2026-06-08 18:13:28 +0530

    refactor(core): switch database setup to SQLAlchemy AsyncEngine and AsyncSession

--- app/core/db.py
+++ app/core/db.py
@@ -1,14 +1,16 @@
 """
-    -------database module-------
-
-    This file for maintaining database
-    database connections and sessions.
+This file for maintaining database
+database connections and sessions.
 """
 
 from typing import Annotated
+
 from fastapi.params import Depends
-from sqlalchemy import create_engine, URL
-from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+# from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+
 from app.core.config import DatabaseConfig
 from app.core.logging_config import get_logger
 
@@ -16,18 +18,22 @@ logger = get_logger(__name__)
 
 db_config = DatabaseConfig()
 
-engine = create_engine(db_config.build_connection(),pool_size=10,pool_timeout=1800)
+engine = create_async_engine(
+    db_config.build_connection(), pool_size=10, pool_timeout=1800
+)
 
 try:
     with engine.connect() as conn:
         logger.info("Engine Connected %s", conn)
 except Exception as e:
-    logger.error(f"Engine Error: %s",e)
+    logger.error("Engine Error: %s", e)
 
-sessionlocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
+sessionlocal = async_sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
 
 class Base(DeclarativeBase):
     pass
+
 
 def get_db():
     db = sessionlocal()
@@ -35,8 +41,9 @@ def get_db():
         yield db
     except:
         db.rollback()
-        raise 
+        raise
     finally:
         db.close()
 
-db_dependency = Annotated[Session,Depends(get_db)]
+
+db_dependency = Annotated[AsyncSession, Depends(get_db)]
