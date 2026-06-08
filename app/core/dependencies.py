7d4aecd80fc2c315be71bad28213667ba5dba262
tahammalik <tahammalik.dev@gmail.com>
2026-06-08 18:26:26 +0530

    fix: correct SQLAlchemy column comparison in conditional

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,60 +1,69 @@
+"""
+This module contains dependencies for the application.
+"""
 
-from fastapi.params import Depends
-from jwt import InvalidTokenError
-from app.services.user_service import find_user
-from app.schemas.token_schema import UserToken
-from app.core.db import db_dependency
-from fastapi import HTTPException,status
-from typing import Annotated, Optional,List
-from app.core.config import SecretConfig
+from typing import Annotated, List, Optional
+
 import jwt
+from fastapi import HTTPException, status
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+
+from app.core.config import SecretConfig
+from app.core.db import db_dependency
 from app.core.exceptions import UserNotFoundError
 from app.models.user_model import User
-from fastapi.security import OAuth2PasswordBearer
+from app.schemas.token_schema import UserToken
+from app.services.user_service import find_user
 
 secrets = SecretConfig()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# to get and verify user by jwt token and return user object
-async def get_current_user(token:Annotated[str,Depends(oauth2_scheme)],db:db_dependency) -> Optional[User]:
-      credentials_exception = HTTPException(
+
+# dependency to get current user from jwt token
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], db: db_dependency
+) -> Optional[User]:
+    credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="could not validate credentials"
-      )
+        detail="could not validate credentials",
+    )
     # decode jwt token and extract user information
-      try:
-        payload = jwt.decode(token,secrets.secret_key,algorithms=[secrets.algorithm])
-        id = payload.get('sub')
-        
+    try:
+        payload = jwt.decode(token, secrets.secret_key, algorithms=[secrets.algorithm])
+        id = payload.get("sub")
         if id is None:
             raise credentials_exception
-        
-        token_data = UserToken(id=int(id))
-      except InvalidTokenError:
-          raise credentials_exception
-      
-      # validate user with data from token
-      user = find_user(token_data.id,db=db)
-      
-      if not user:
-          raise UserNotFoundError(message=f"user not found!")
-      if user.is_active == False:
-          raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                              detail='user is suspended or deactivated')
-      
-      return user
 
-# dependency to check manager or admin
-def require_role(allowed_role:List[str]):
-    # actual function that used by fastapi
-    async def role_checker(current_user:User = Depends(get_current_user)):
+        token_data = UserToken(id=int(id))
+    except InvalidTokenError:
+        raise credentials_exception
+
+    # find user by id from token data
+    user = find_user(token_data.id, db=db)
+
+    if not user:
+        raise UserNotFoundError(message="user not found!")
+    if user.is_active is False:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="user is suspended or deactivated",
+        )
+
+    return user
+
+
+# dependency to check user role
+def require_role(allowed_role: List[str]):
+    # checks if the user has one of the allowed roles
+    async def role_checker(current_user: Annotated[User, Depends(get_current_user)]):
         if current_user.role not in allowed_role:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail='You do not have permission to perform this action'
+                detail="You do not have permission to perform this action",
             )
         return current_user
+
     return role_checker
-
-
