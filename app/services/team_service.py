1391e42ebd059eefa723d5f86a7000529456c26c
tahammalik <tahammalik.dev@gmail.com>
2026-06-08 18:22:58 +0530

    refactor(core): use async to function create_team

--- app/services/team_service.py
+++ app/services/team_service.py
@@ -1,26 +1,25 @@
-from app.core.db import db_dependency
-from app.schemas.team_schema import CreateTeam
-from app.models.team_model import Team
 from logging import getLogger
-from fastapi import HTTPException,status
+
+from fastapi import HTTPException, status
+
+from app.core.db import db_dependency
+from app.models.team_model import Team
+from app.schemas.team_schema import CreateTeam
 
 logger = getLogger(__name__)
 
 
-def create_team(group_data:CreateTeam,db:db_dependency):
+async def create_team(group_data: CreateTeam, db: db_dependency):
 
-    new_group = Team(
-        team_name = group_data.team_name,
-        description = group_data.description
-    )
+    new_group = Team(team_name=group_data.team_name, description=group_data.description)
 
     try:
         db.add(new_group)
-        db.commit()
-        db.refresh(new_group)
+        await db.commit()
+        await db.refresh(new_group)
     except Exception as e:
-        logger.error(f"DB ERROR: %s", e)
+        logger.error("DB ERROR: %s", e)
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Database error occurred"
+            detail="Database error occurred",
         )
