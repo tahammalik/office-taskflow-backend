7db78c340afc3c0316bd62cdc21406a8b5acd0ff
tahammalik <tahammalik.dev@gmail.com>
2026-06-08 18:06:55 +0530

    fix: correct type hint in build_connection function

--- app/core/config.py
+++ app/core/config.py
@@ -1,45 +1,45 @@
 """
-    This file maintain configs like secretkeys,passwords, and database related info
+This file maintain configs like secretkeys,passwords, and database related info
 """
-from pydantic_settings import BaseSettings,SettingsConfigDict
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy import URL
 
+
+# Here we define class for database connection info
 class DatabaseConfig(BaseSettings):
-    # define components for database connection
+    # take database connection info from .env file
     model_config = SettingsConfigDict(
-        env_file='.env',
-        env_file_encoding='utf-8',
-        case_sensitive=False
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=False
     )
 
-    drivername:str
-    username:str
-    password:str
-    host:str
-    port:int
-    database:str
+    drivername: str
+    username: str
+    password: str
+    host: str
+    port: int
+    database: str
 
     # build connection with database
-    def build_connection(self) -> str:
+    def build_connection(self) -> URL:
         return URL.create(
             drivername=self.drivername,
             username=self.username,
             password=self.password,
             host=self.host,
             port=self.port,
-            database=self.database
+            database=self.database,
         )
-# Here we define class for importent keys and passwords
+
+
+# Here we define class for secrete keys and passwords
 class SecretConfig(BaseSettings):
+    # take secret keys and passwords from .key file
     model_config = SettingsConfigDict(
-        env_file='.key',
-        env_file_encoding='utf-8',
-        case_sensitive=False
+        env_file=".key", env_file_encoding="utf-8", case_sensitive=False
     )
 
-    password_secret_key:str     # for password, makes passwords more secure by adding this key to the password before hashing
-    dummy_hash:str              # dummy hash for password comaprison
-    secret_key:str              # for jwt token, should be long and random  
-    algorithm:str               # for jwt token
-
-
+    password_secret_key: str  # for password, makes passwords more secure by adding this key to the password before hashing
+    dummy_hash: str  # dummy hash for password comaprison
+    secret_key: str  # for jwt token, should be long and random
+    algorithm: str  # for jwt token
