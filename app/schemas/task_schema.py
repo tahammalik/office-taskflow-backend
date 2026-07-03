f21068775febe02c7ce0700da31e002737fa7964
tahammalik <tahammalik.dev@gmail.com>
2026-07-03 21:45:28 +0530

    fix: TaskResponse schema and taskhistroy schema

--- app/schemas/task_schema.py
+++ app/schemas/task_schema.py
@@ -9,21 +9,29 @@ from app.schemas.user_schema import UserMinRead
 # schema for create task
 class CreateTask(BaseModel):
     title: str = Field(..., max_length=50)
-    description: Optional[str] = Field(None)
+    description: str
     status: Literal["todo", "in_progress", "review", "done"]
     dead_line: datetime
     team_id: int
     assign_to: int  # user id of the employee to whom the task is assigned
 
+class TaskHistory(BaseModel):
+    task_id: int
+    changed_by: int
+    old_status: str
+    new_status: str
+    comment: str
+    changed_at: datetime
 
 # Task response schema
 class ResponseTask(BaseModel):
     id: int
     title: str
-    description: Optional[str]
+    description: str
     status: str
     assigned_employee: UserMinRead
     creator_manager: UserMinRead
+    task_history: TaskHistory
 
     model_config = ConfigDict(from_attributes=True)
 
@@ -34,3 +42,6 @@ class TaskUpdate(BaseModel):
     description: Optional[str] = None
     assign_to: Optional[int] = None  # param:id so datatype is int
     status: Optional[Literal["todo", "in_progress", "review", "done"]] = None
+    status_comment: Optional[str] = None
+    dead_line: Optional[datetime] = None
+
