3a219444b9396808222fbc1c7925c13086d8421b
tahammalik <tahammalik.dev@gmail.com>
2026-07-05 00:06:21 +0530

    feat:add refresh token and logout,refresh endpoint

--- app/api/v1/authentication.py
+++ app/api/v1/authentication.py
@@ -1,21 +1,24 @@
 """
 This is file handle authentication router
 """
-
+import jwt
 from typing import Annotated
-
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status,Response,Request
 from fastapi.security import OAuth2PasswordRequestForm
 from app.core.db import db_dependency
 from app.core.dependencies import get_current_user
 from app.core.exceptions import EmailAlreadyExistsError
 from app.core.logging_config import get_logger
-from app.core.security import create_access_token, hash_password,create_refresh_token
+from app.core.security import (create_access_token,
+                               hash_password,
+                               create_refresh_token,
+                               is_refresh_token_valid_and_revoke,
+                               decode_token,
+                               revoke_refresh_token)
 from app.models.user_model import User
-from app.models.refresh_token import RefreshToken
-from app.schemas.token_schema import Token,TokenResponse
 from app.schemas.user_schema import UserCreate, UserResponse
 from app.services.authentication_service import authenticate_user, find_email
+
 
 
 # from app.core.dependencies import require_role, get_current_user
@@ -26,9 +29,7 @@ router = APIRouter(prefix="/v1/auth", tags=["Authentication"])
 
 
 # Signup endpoint for creating a new user
-@router.post(
-    "/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED
-)
+@router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def create_user(user_data: UserCreate, db: db_dependency):
 
     email = await find_email(user_data.email, db=db)
@@ -57,9 +58,7 @@ async def create_user(user_data: UserCreate, db: db_dependency):
 
 
 @router.post("/login")
-async def login(
-        form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
-):
+async def login(response:Response,form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
 
     user = await authenticate_user(form_data.username, form_data.password, db=db)
 
@@ -73,11 +72,55 @@ async def login(
     access_token = await create_access_token(data={"sub": str(user.id)})
     refresh_token = await create_refresh_token(data={"sub": str(user.id)})
 
-    return TokenResponse(
-        access_token=access_token,
-        refresh_token=refresh_token,
-        token_type="bearer",
+    response.set_cookie(
+        key="refresh_token",
+        secure=True,
+        value=refresh_token,
+        httponly=True,
+        samesite="strict",
+        max_age=7 * 24 * 60 * 60
     )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post("/refresh")
+async def refresh(request:Request,response:Response):
+    refresh_token = request.cookies.get("refresh_token")
+    if not refresh_token:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Missing refresh token"
+            )
+    payload = decode_token(token=refresh_token,expected_type="refresh")
+    jti = payload.get("jti")
+    username = payload.get("sub")
+    if not is_refresh_token_valid_and_revoke(jti=jti):
+        raise HTTPException(status_code=401, detail="Refresh token invalid or already used")
+    new_access = create_access_token(data={"sub": username})
+    new_refresh = create_refresh_token(data={"sub": username})
+
+    response.set_cookie(
+         key="refresh_token",
+        secure=True,
+        value=refresh_token,
+        httponly=True,
+        samesite="strict",
+        max_age=7 * 24 * 60 * 60
+    )
+
+    return {"access_token": new_access, "token_type": "bearer"}
+
+@router.post("/logout")
+async def logout(request:Request,response:Response):
+    token = request.cookies.get("refresh_token")
+    if token:
+        try:
+            payload = decode_token(token,expected_type="refresh")
+            revoke_refresh_token(payload["jti"])
+        except Exception:
+            pass
+    response.delete_cookie("refresh_token")
+    return {"detail":"logout"}
 
 @router.get("/me", response_model=UserResponse)
 async def get_me(current_user: Annotated[User, Depends(get_current_user)]):
