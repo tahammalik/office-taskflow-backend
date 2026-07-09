1877d0cf1bf5e25af751d26b69fe93c8a2e69855
tahammalik <tahammalik.dev@gmail.com>
2026-07-10 00:40:26 +0530

    feat: add project histroy table and project status imporove code quality

--- app/models/project_model.py
+++ app/models/project_model.py
@@ -1,6 +1,10 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func, Table, Boolean
+from sqlalchemy import (Column,Integer,String,
+                        DateTime,ForeignKey,
+                        func, Table,Boolean, Text)
+from sqlalchemy import Enum as SAEnum
 from sqlalchemy.orm import relationship
 from app.core.db import Base
+import enum
 
 # Association table for Project and Team (Many-to-Many)
 ProjectTeams = Table(
@@ -10,18 +14,43 @@ ProjectTeams = Table(
     Column('team_id', Integer, ForeignKey('teams.id'), primary_key=True)
 )
 
+class ProjectStatus(str,enum.Enum):
+    PLANNING = "planning"
+    ACTIVE = "active"
+    ON_HOLD = "on_hold"
+    REVIEW = "review"
+    COMPLETED = "completed"
+    ARCHIVED = "archived"
+
 class Project(Base):
     __tablename__ = 'projects'
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    description = Column(String)
+    title = Column(String(250), nullable=False)
+    description = Column(Text)
     deadline = Column(DateTime)
-    is_active = Column(Boolean, default=True)
+    status = Column(SAEnum(ProjectStatus,name='project_status_enum'),nullable=False,default=ProjectStatus.PLANNING)
+    is_deleted = Column(Boolean, default=False)
     created_at = Column(DateTime, default=func.now())
-    created_by = Column(Integer, ForeignKey('users.id'))
-    workspace_id = Column(Integer, ForeignKey('workspaces.id'))
+    created_by = Column(Integer, ForeignKey('users.id'),index=True)
+    workspace_id = Column(Integer, ForeignKey('workspaces.id'),index=True)
 
     # Relationships
     workspace = relationship('Workspace', back_populates='projects')
     teams = relationship('Team', secondary="project_teams", back_populates='projects')
+    history = relationship('ProjectHistory',back_populates='project')
+
+class ProjectHistory(Base):
+    __tablename__ = "project_histories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    project_id = Column(Integer, ForeignKey("projects.id", ondelete="CASCADE"), nullable=False,index=True)
+    changed_by = Column(Integer, ForeignKey("users.id"), nullable=False)
+    field_name = Column(String(50), nullable=False)   # e.g., "status", "title"
+    old_value = Column(Text, nullable=True)
+    new_value = Column(Text, nullable=True)
+    changed_at = Column(DateTime, default=func.now(), nullable=False,index=True)
+
+    # Relationships
+    project = relationship("Project", back_populates="history")
+    user = relationship("User", back_populates="project_history")
